2b2adbf4b638852996a50082e46d6a1ce6460cfd
dbrcina <dbrcina@donotreply.com>
2021-03-30 23:17:17 +0100

    Done?

--- dz1/src/main/java/hr/fer/zpm/tg/dz1/Demo.java
+++ dz1/src/main/java/hr/fer/zpm/tg/dz1/Demo.java
@@ -5,20 +5,48 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Locale;
 
 public class Demo {
 
     public static void main(String[] args) throws IOException {
+        long start = System.nanoTime();
         if (args.length != 1) {
             System.out.println("Program expects a path to graph definition file !");
             return;
         }
 
         Graph g = Graph.fromFile(args[0]);
-        System.out.println();
+        int path = findLongestPath(g);
+        System.err.printf(Locale.US, "Runtime: %.5fs%n", (System.nanoTime() - start) * 1e-9);
+        System.out.println("Longest path = " + path);
     }
 
     private static int findLongestPath(Graph g) {
+        int vertices = g.getVertices();
+        int[][] adjacencyM = g.getAdjacencyM();
+        int path = 0;
+        BitSet visited = new BitSet(vertices);
+        for (int i = 0; i < vertices; i++) {
+            path = Math.max(path, dfs(i, vertices, adjacencyM, visited));
+            if (visited.cardinality() == vertices) {
+                // if every vertex has been visited i.e Hamiltonian path
+                break;
+            } else { // reset visited set
+                visited.clear();
+            }
+        }
+        return path;
+    }
+
+    private static int dfs(int i, int vertices, int[][] adjacencyM, BitSet visited) {
+        visited.set(i);
+        for (int j = 0; j < vertices; j++) {
+            if (!visited.get(j) && adjacencyM[i][j] == 1) {
+                return 1 + dfs(j, vertices, adjacencyM, visited);
+            }
+        }
         return 0;
     }
 
@@ -37,9 +65,7 @@ public class Demo {
         }
 
         public int[][] getAdjacencyM() {
-            return Arrays.stream(adjacencyM)
-                    .map(int[]::clone)
-                    .toArray(int[][]::new);
+            return adjacencyM;
         }
 
         public static Graph fromFile(String file) throws IOException {
